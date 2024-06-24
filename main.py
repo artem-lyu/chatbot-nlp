2c7e5f5999d364c6179c90ecf89e40bf5fef4b3c
artem-lyu <lyuartem@gmail.com>
2024-06-24 09:16:11 -0500

    finished chatbot

--- main.py
+++ main.py
@@ -65,3 +65,48 @@ model.compile(loss = 'categorical_crossentropy',
 print(model.summary())
 model.fit(x=train_X, y=train_Y, epochs=150, verbose = 1)
 
+def clean_text(text):
+    tokens = nltk.word_tokenize(text)
+    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+    return tokens
+
+def bag_of_words(text, vocab):
+    tokens = clean_text(text)
+    bow = [0] * len(vocab)
+    for w in tokens:
+        for idx, word in enumerate(vocab):
+            if word == w:
+                bow[idx] = 1
+    return np.array(bow)
+
+def pred_class(text, vocab, labels):
+    bow = bag_of_words(text, vocab)
+    result = model.predict(np.array([bow]))[0]
+    thresh = 0.5
+    y_pred = [[indx, res] for indx, res in enumerate(result) if res > thresh]
+    y_pred.sort(key=lambda x:x[1], reverse=True)
+    return_list = []
+    for r in y_pred:
+        return_list.append(labels[r[0]])
+    return return_list
+
+def get_response(intents_list, intents_json):
+    if len(intents_list) == 0:
+        result = "Sorry, can't comprehend!"
+    else:
+        tag = intents_list[0]
+        list_of_intents = intents_json["intents"]
+        for i in list_of_intents:
+            if i["tag"] == tag:
+                result = random.choice(i["responses"])
+                break
+    return result
+
+print("Press 0 to stop at anytime")
+while True:
+    message = input("")
+    if message =="0":
+        break
+    intents = pred_class(message, words, classes)
+    result = get_response(intents, data)
+    print(result)
